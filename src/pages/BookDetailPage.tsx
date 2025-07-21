5ec7bbc267d7e496dc8ad82598776329128c1153
Eco-15 <103921235+Eco-15@users.noreply.github.com>
2025-07-21 18:27:34 -0500

    Updated BookDetailPage.tsx

--- src/pages/BookDetailPage.tsx
+++ src/pages/BookDetailPage.tsx
@@ -2071,7 +2071,7 @@ For insurance professionals, Wooden's leadership principles provide a framework 
               {/* Book Content */}
               <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
                 {/* Main Content */}
-                <div className="lg:col-span-1 space-y-20">
+                <div className="lg:col-span-2 space-y-8">
                   
 
                   {/* Key Takeaways */}
