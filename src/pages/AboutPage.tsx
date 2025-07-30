acc7f28ade54da8895531e30e14385f73f069378
Eco-15 <103921235+Eco-15@users.noreply.github.com>
2025-07-30 16:07:44 -0500

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -111,7 +111,7 @@ const AboutPage = () => {
               src="https://lyz5cvfr0h.ufs.sh/f/tLx4hl5ikmOnngnqsvftEwlRJSPX6gNfrmvuq2WM4xeFGATH"
               alt="EIB Team joins PHP Agency"
              className="rounded-lg object-cover w-full aspect-video shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-             style={{ width: '768px', height: '1024px', maxWidth: '100%', height: 'auto' }}
+             style={{ width: '1024px', height: '768px', maxWidth: '100%', height: 'auto' }}
             />
           </div>
         </div>
