7e663bd5ad4c72a83f3fbdd92ae67a9edc5f843f
Eco-15 <103921235+Eco-15@users.noreply.github.com>
2025-08-07 14:22:06 -0500

    Updated AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -359,12 +359,12 @@ const AboutPage = () => {
                   <div className="bg-white rounded-xl shadow-lg border border-gray-200 p-4 max-w-xs hover:shadow-xl transition-all duration-300 hover:scale-105">
                     <div className="relative rounded-lg overflow-hidden mb-3">
                       <img
-                        src="https://lyz5cvfr0h.ufs.sh/f/tLx4hl5ikmOnq9RCcI0f0rumkpwBXbZgQ169Mj5tITK2SiFY"
+                        src="https://lyz5cvfr0h.ufs.sh/f/tLx4hl5ikmOncG6JNkXABiZ5cudwgVeLkIK6oqazNlQhtEn2"
                         alt="Builders Bowl 2019"
                         className="w-full h-20 md:h-80 object-cover"
                       />
                     </div>
-                    <h3 className="text-sm md:text-base font-bold text-black text-center"> Meeting George Bush</h3>
+                    <h3 className="text-sm md:text-base font-bold text-black text-center">2019 - Builders Bowl</h3>
                   </div>
                 </div>
               </div>
