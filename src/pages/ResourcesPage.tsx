2466d1e148467fce298524917882518db9426b40
Eco-15 <103921235+Eco-15@users.noreply.github.com>
2025-08-15 10:46:50 -0500

    Updated ResourcesPage.tsx

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -171,7 +171,7 @@ const ResourcesPage = () => {
       description: 'Follow our women\'s empowerment and leadership development account.',
       category: 'support',
       type: 'Social Media',
-     icon: () => <img src="https://a479266c5n.ufs.sh/f/shgffwCK3PoBRWlGICOgVTcCQpk6nHjugZ8JUy5LYNeKEvhB" alt="Instagram" className="h-10 w-10" />,
+     icon: () => <img src="https://a479266c5n.ufs.sh/f/shgffwCK3PoBRWlGICOgVTcCQpk6nHjugZ8JUy5LYNeKEvhB" alt="Instagram" className="h-6 w-6" />,
       link: 'https://www.instagram.com/iconic.women._?igsh=eGxzenJpYmFoYWJv',
       featured: false
     },
