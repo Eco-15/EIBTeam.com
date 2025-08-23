a7ba9db1a55035df14cb7b94b5b9e1f6295ca124
Eco-15 <103921235+Eco-15@users.noreply.github.com>
2025-08-23 00:30:02 -0500

    Updated TrainingDetailPage.tsx

--- src/pages/TrainingDetailPage.tsx
+++ src/pages/TrainingDetailPage.tsx
@@ -528,7 +528,7 @@ const TrainingDetailPage = () => {
       difficulty: 'Intermediate',
       rating: 5.0,
       instructor: 'Jason Graziani',
-      video: '', // To be integrated
+      video: 'https://lyz5cvfr0h.ufs.sh/f/tLx4hl5ikmOnpJxnAeI6DARSxfOkQHXiIBqK1FClcrjMG02u', // To be integrated
       thumbnail: 'https://lyz5cvfr0h.ufs.sh/f/tLx4hl5ikmOnWyr2rmwDoEgx6u7dLy8etIN1pvK3aPWcf5AR',
       objectives: [
         'Handle client questions',
