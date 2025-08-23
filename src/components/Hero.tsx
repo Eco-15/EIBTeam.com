74bd94f052580eaa16d9b268125c5c1699185326
Eco-15 <103921235+Eco-15@users.noreply.github.com>
2025-08-22 20:59:49 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -101,10 +101,6 @@ const Hero = () => {
             </div>
             <div className="flex items-center space-x-2">
               <div className="w-2 h-2 bg-green-500 rounded-full"></div>
-              <span>Licensed in Texas</span>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div className="w-2 h-2 bg-green-500 rounded-full"></div>
               <span>24/7 Support</span>
             </div>
           </div>
